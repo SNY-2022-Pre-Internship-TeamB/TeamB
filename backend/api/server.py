e190c66e2ee16efa5335a52bb8a3751debadc4ef
Jo Byeong Wook <whquddnr97@gmail.com>
2022-02-13 06:40:06 +0900

    added routes (search policy name, policy detail)

--- backend/api/server.py
+++ backend/api/server.py
@@ -1,19 +1,9 @@
 from fastapi import FastAPI
 from prometheus_fastapi_instrumentator import Instrumentator
-from model import *
+from routes.policy_name import router as PolicyNameRouter
+from routes.policy_details import router as PolicyDetailRouter
 
 app = FastAPI()
+app.include_router(PolicyNameRouter, tags = ["Policy Name"], prefix = "/policy")
+app.include_router(PolicyDetailRouter, tags = ["Policy Detail"], prefix = "/detail")
 Instrumentator().instrument(app).expose(app)
-
-@app.get("/")
-def test_func():
-    return "Test Func"
-
-@app.post("/nums/")
-async def policy_num(item : policy_name):
-    test_dict = item.dict()
-    if item.u_region == "서울":
-        test_dict.update({"정책 번호" : "R0001"})
-    if item.u_region == "기타":
-        test_dict.update({"정책 번호": "R0002"})
-    return test_dict
