63aeeb640063cc996de5d75527ef209a9290db83
Jo Byeong Wook <whquddnr97@gmail.com>
2022-02-13 07:27:20 +0900

    added CORS middleware

--- backend/api/server.py
+++ backend/api/server.py
@@ -1,9 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from prometheus_fastapi_instrumentator import Instrumentator
 from routes.policy_name import router as PolicyNameRouter
 from routes.policy_details import router as PolicyDetailRouter
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = ["*"],
+    allow_credentials = True,
+    allow_methods = ["*"],
+    allow_headers = ["*"]
+)
+
 app.include_router(PolicyNameRouter, tags = ["Policy Name"], prefix = "/policy")
 app.include_router(PolicyDetailRouter, tags = ["Policy Detail"], prefix = "/detail")
+
 Instrumentator().instrument(app).expose(app)
