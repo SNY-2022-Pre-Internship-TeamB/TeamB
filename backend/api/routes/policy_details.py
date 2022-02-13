a13b9b7a02d8b3566cbfc859170bae184f7b2538
Jo Byeong Wook <whquddnr97@gmail.com>
2022-02-13 21:11:29 +0900

    added root routes

--- backend/api/routes/policy_details.py
+++ backend/api/routes/policy_details.py
@@ -5,11 +5,17 @@ from fastapi import Query
 
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 
-from database import retrieve_details
+from database import retrieve_details, retrieve_all_details
 
 router = APIRouter()
 
-@router.get("", response_description = "Get Policy Detail")
+@router.get("", response_description = "모든 정책의 상세정보 조회")
+async def get_all_policy_detail():
+    details = await retrieve_all_details()
+    if details:
+        return details
+
+@router.get("/", response_description = "해당하는 정책번호의 상세정보 조회")
 async def get_policy_detail(policy_number : str = Query(None, description = "정책 번호")):
     details = await retrieve_details(policy_number)
     if details:
