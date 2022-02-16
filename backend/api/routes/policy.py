57ac66d6d92a25550be5da39d55d085ef1f993a2
Jo Byeong Wook <whquddnr97@gmail.com>
2022-02-17 08:17:25 +0900

    added DELETE, OPTIONS, HEAD method

--- backend/api/routes/policy.py
+++ backend/api/routes/policy.py
@@ -1,8 +1,9 @@
 import sys
 import os
 from typing import Optional
-from fastapi import APIRouter, Body, status, BackgroundTasks
+from fastapi import APIRouter, Body, status, BackgroundTasks, Response
 from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
 from fastapi import Query
 
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
@@ -11,7 +12,7 @@ from model import *
 
 router = APIRouter()
 
-@router.get("", responses = {200 : {"model" : PolicySchema},
+@router.get("", responses = {200 : {"model" : PolicyNameSchema},
                              404 : {"model" : ErrorResponseModel}})
 async def get_policy(u_region : Optional[str] = Query(None, description = "사용자의 지역"),
                      u_age : Optional[int] = Query(None, description = "사용자의 나이"),
@@ -28,15 +29,18 @@ async def get_policy(u_region : Optional[str] = Query(None, description = "사�
         return errorResponse(404, "There are no policies")
 
 @router.post("", responses = {202 : {"model" : AsyncResponseModel}})
-async def post_policy(background_tasks : BackgroundTasks, policy : PolicySchema = Body(...)):
+async def post_policy(background_tasks : BackgroundTasks, policy : PolicyNameSchema = Body(...)):
     policy = jsonable_encoder(policy)
     background_tasks.add_task(add_policy, policy)
 
     return asyncResponse(202, "GET", "http://localhost:8000/policies/{}/details".format(policy['policy_id']))
 
 @router.delete("", status_code = status.HTTP_204_NO_CONTENT, response_description = "Successfully deleted")
-async def delete_policy(background_tasks : BackgroundTasks):
-    background_tasks.add_task(delete_all_policy)
+async def delete_policy(background_tasks : BackgroundTasks, policy : PolicyNameSchema = Body(...)):
+    policy = jsonable_encoder(policy)
+    background_tasks.add_task(delete_policy_by, policy)
+
+    return
 
 @router.put("")
 async def put_policy():
@@ -47,9 +51,21 @@ async def patch_policy():
     pass
 
 @router.options("")
-async def options_policy():
-    pass
+async def options_policy(response : Response):
+    response.headers["Allow"] = "GET, PUT, POST, DELETE, OPTIONS, HEAD, PATCH"
 
-@router.head("")
-async def head_policy():
-    pass
+@router.head("", responses = {200 : {},
+                              404 : {}})
+async def head_policy(u_region : Optional[str] = Query(None, description = "사용자의 지역"),
+                      u_age : Optional[int] = Query(None, description = "사용자의 나이"),
+                      policy_type : Optional[str] = Query(None, description = "사용자가 선택한 정책 유형")):
+
+    if u_region and u_age and policy_type:
+        policies = await retrieve_policies_by_user(u_region, u_age, policy_type)
+    else:
+        policies = await retrieve_all_policies()
+
+    if policies:
+        return JSONResponse(status_code = 200)
+    else:
+        return JSONResponse(status_code = 404)
