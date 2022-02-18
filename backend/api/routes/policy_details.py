38ea27b764489e2a2e51e38a6cc4b5ab998d30e5
Jo Byeong Wook <whquddnr97@gmail.com>
2022-02-18 10:10:23 +0900

    added HTTP method

--- backend/api/routes/policy_details.py
+++ backend/api/routes/policy_details.py
@@ -1,6 +1,9 @@
 import sys
 import os
-from fastapi import APIRouter, Query, Path
+from fastapi import APIRouter, Body, status, BackgroundTasks, Response
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
+from fastapi import Path
 
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 from database import *
@@ -20,3 +23,46 @@ async def get_policy_detail(policy : str = Path(..., description = "정책 번�
         return okResponse(200, details)
     else:
         return errorResponse(404, "There are no policies")
+
+@router.post("", responses = {202 : {"model" : AsyncResponseModel}})
+async def post_policy(background_tasks : BackgroundTasks, policy : str = Path(..., description = "정책 번호 또는 이름"),
+                      policy_post : PolicySchema = Body(...)):
+    policy_post = jsonable_encoder(policy_post)
+    background_tasks.add_task(add_policy, policy_post)
+
+    return asyncResponse(202, "GET", "http://localhost:8000/policies/{}".format(policy))
+
+@router.delete("", status_code = status.HTTP_204_NO_CONTENT, response_description = "Successfully deleted")
+async def delete_policy(background_tasks : BackgroundTasks, policy : str = Path(..., description = "정책 번호 또는 이름")):
+    if policy.startswith("R"):
+        background_tasks.add_task(delete_policy_by_id, policy)
+    else:
+        background_tasks.add_task(delete_policy_by_name, policy)
+
+    return JSONResponse(status_code = 204)
+
+@router.options("")
+async def options_policy(response : Response, policy : str = Path(..., description = "정책 번호 또는 이름")):
+    response.headers["Allow"] = "GET, PUT, POST, DELETE, OPTIONS, HEAD, PATCH"
+
+@router.head("", responses = {200 : {},
+                              404 : {}})
+async def head_policy(policy : str = Path(..., description = "정책 번호 또는 이름")):
+
+    if policy.startswith("R"):
+        details = await retrieve_details_by_id(policy)
+    else:
+        details = await retrieve_details_by_name(policy)
+
+    if details:
+        return JSONResponse(status_code = 200)
+    else:
+        return JSONResponse(status_code = 404)
+
+@router.put("")
+async def put_policy(background_tasks : BackgroundTasks, policy : str = Path(..., description = "정책 번호 또는 이름")):
+    pass
+
+@router.patch("")
+async def patch_policy(policy : str = Path(..., description = "정책 번호 또는 이름")):
+    pass
